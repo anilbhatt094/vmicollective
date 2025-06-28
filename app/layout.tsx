09066b05feb25984b36db31874a620f92a7e793a
Anil Bhatt <67644691+anilbhatt094@users.noreply.github.com>
2025-06-28 21:52:27 +0530

    onClick remove

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React , {useRef} from "react"
+import React  from "react"
 import "@/styles/globals.css";
 import Header from "../components/common/Header"
 import Footer from "../components/common/Footer"
@@ -12,16 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
 
-const sectionOneRef = useRef<HTMLDivElement | null>(null);
-  const sectionTwoRef = useRef<HTMLDivElement | null>(null);
 
-  const scrollToSection = (section: "one" | "two") => {
-    if (section === "one" && sectionOneRef.current) {
-      sectionOneRef.current.scrollIntoView({ behavior: "smooth" });
-    } else if (section === "two" && sectionTwoRef.current) {
-      sectionTwoRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  };
 
   return (
     <html suppressHydrationWarning lang="en">
@@ -30,7 +21,7 @@ const sectionOneRef = useRef<HTMLDivElement | null>(null);
         
       >
         <>
-        <Header onMenuClick={scrollToSection}/>
+        <Header />
          <main>
               {children}
             </main>
